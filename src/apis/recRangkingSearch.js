e0b2b9d241c9b0a802d049486b299ccd48956529
박세현 <sh.park@mindwarewokrs.com>
2023-04-12 14:06:32 +0900

    t

--- src/apis/recRangkingSearch.js
+++ src/apis/recRangkingSearch.js
@@ -18,7 +18,10 @@ async function recRangkingSearch(nm,callback) {
           const url = "https://www.10000recipe.com"+$(el).find('.common_sp_thumb a').attr('href')
           const title = $(el).find('.common_sp_caption_tit').text().trim();
           const imgUrl = $(el).find('.common_sp_thumb a > img').attr('src')
-          data.push({rangking:i+1,url,title,imgUrl});
+          const starCnt = $(el).find('.common_sp_caption_rv span').first().children().length;
+          const starUrl = "https://recipe1.ezmember.co.kr/img/mobile/icon_star2_on.png"
+          const viewsCnt = $(el).find('.common_sp_caption_buyer').text().trim();
+          data.push({rangking:i+1,url,title,imgUrl,starCnt,starUrl,viewsCnt});
         });
         callback(null, data);
       } else {
